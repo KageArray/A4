0f02635dcf266e9e4ad6db1ed16c4f8983095ea1
Kristoffer <bvj755@alumni.ku.dk>
2016-12-05 02:44:30 +0100

    Update usr_sem.c

--- A4/kudos/kudos/proc/usr_sem.c
+++ A4/kudos/kudos/proc/usr_sem.c
@@ -1,1 +1,15 @@
-/// something here
+#include "kernel/interrupt.h"
+#include "kernel/semaphore.h"
+#include "kernel/sleepq.h"
+#include "kernel/config.h"
+#include "kernel/assert.h"
+#include "lib/libc.h"
+#include "proc/usr_sem.h"
+
+/// defining an array of entries and initialize them
+void sem_init() {
+  int i;
+  for (i = 0; i < CONFIG_MAX_SEMAPHORES; ++i) {
+    semaphore_table[i].status = -1;
+  }
+}
