f60e3d7c9896d65b796f0d02b29be34eaa335c65
Kristoffer <kristofferthoroe@gmail.com>
2016-12-06 16:45:21 +0100

    tests

---
+++ A4/kudos/userland/join_exit_test.c
@@ -0,0 +1,18 @@
+#include "lib.h"
+
+
+int main(){
+  int tid;
+  int retval;
+  tid = syscall_spawn("[disk]proc", 0);
+  retval = (syscall_join(tid));
+  printf("retval = %d\n",retval);
+  if (retval == 80085){
+    printf("Test of syscall_join and syscall_exit succeeded\n");
+  }
+  else{
+    printf("Test of syscall_join and syscall_exit failed\n");
+  }
+  syscall_halt();
+  return (0);
+}
---
+++ A4/kudos/userland/proc.c
@@ -0,0 +1,6 @@
+#include "lib.h"
+
+int main(){
+  syscall_exit(80085);
+  return 0;
+}
